9400538fce149af1549b3e228f1f31572a06cba6
Josef David <josefmarydavid@gmail.com>
2018-03-18 16:45:22 +0800

    fixed index and main .php

--- main.php
+++ main.php
@@ -8,79 +8,6 @@ if(session_id() == '' || !isset($_SESSION)){
 <html ng-app="myApp">
 <head>
   <?php include('assets/head.html') ?>
-  <style>
-  body {font-family: Arial, Helvetica, sans-serif;}
-
-/* The Modal (background) */
-.modal {
-    display: none; /* Hidden by default */
-    position: fixed; /* Stay in place */
-    z-index: 1; /* Sit on top */
-    padding-top: 100px; /* Location of the box */
-    left: 0;
-    top: 0;
-    width: 100%; /* Full width */
-    height: 100%; /* Full height */
-    overflow: auto; /* Enable scroll if needed */
-    background-color: rgb(0,0,0); /* Fallback color */
-    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-}
-
-/* Modal Content */
-.modal-content {
-    position: relative;
-    background-color: #fefefe;
-    margin: auto;
-    padding: 0;
-    border: 1px solid #888;
-    width: 50%;
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
-    -webkit-animation-name: animatetop;
-    -webkit-animation-duration: 0.4s;
-    animation-name: animatetop;
-    animation-duration: 0.4s
-}
-
-/* Add Animation */
-@-webkit-keyframes animatetop {
-    from {top:0px; opacity:0}
-    to {top:0; opacity:1}
-}
-
-@keyframes animatetop {
-    from {top: 0px; opacity:0}
-    to {top:0; opacity:1}
-}
-
-/* The Close Button */
-.close {
-    color: black;
-    float: right;
-    font-size: 28px;
-    font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-    color: #000;
-    text-decoration: none;
-    cursor: pointer;
-}
-
-.modal-header {
-    padding: 2px 16px;
-    background-color: white;
-    color: white;
-}
-
-.modal-body {padding: 2px 16px;}
-
-.modal-footer {
-    padding: 2px 16px;
-    background-color: #fff;
-    color: white;
-}
-  </style>
 </head>
   <body>
     <!-- Content of the the top -->
@@ -141,73 +68,11 @@ if(session_id() == '' || !isset($_SESSION)){
   <!-- Gallery image -->
   <?php include('assets/gallery.html') ?>
   <!-- Text list (feautured news) -->
-  <?php include('assets/featured-news.html') ?>
+
   <!-- Footer -->
   <?php include('assets/footer.html') ?>
-
-  <h2>Animated Modal with Header and Footer</h2>
-
-  <!-- Trigger/Open The Modal -->
-  <button id="myBtn">Open Modal</button>
-<div class="border">
-  <img id="myBtn" width="300px" height="300px" src="img/featured-2.jpeg" />
-</div>
-  <!-- The Modal -->
-  <div id="myModal" class="modal">
-
-    <!-- Modal content -->
-    <div class="modal-content">
-      <div class="modal-header">
-        <span class="close">&times;</span>
-        <!-- <h2>Modal Header</h2> -->
-      </div>
-      <div class="modal-body">
-        <div class="image-conten">
-          <img width="400px" height="350px" style="object-fit: cover;"src="img/watch-1.jpeg" alt="sample11" />
-          <div class="text-holder">
-            <div>
-    <p><a href="update-cart.php?action=add&id=1"><input class="add" type="submit" value="Add To Cart"/></a></p>';
-            <div>
-              <p>{{price}}</p>
-            </div>
-            <a href="#"></a>
-          </div>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <!-- <h3>Modal Footer</h3> -->
-      </div>
-    </div>
-  </div>
-
-
-  <script>
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
+  <!-- modal content -->
+  <?php include('assets/modal.html') ?>
   <!-- Scripts  -->
   <?php include('assets/scripts.html') ?>
   </body>
