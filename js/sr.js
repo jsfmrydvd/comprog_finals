54587186746bae190e004d72746512d549f50a69
Josef David <josefmarydavid@gmail.com>
2018-03-18 16:47:27 +0800

    removed some keyframes

--- js/sr.js
+++ js/sr.js
@@ -1,28 +1,5 @@
 window.sr = ScrollReveal();
-// sr.reveal('.navbar', {
-//   duration: 2000,
-//   origin:'top',
-//   distance: '10px'
-// });
-sr.reveal('.showcase-left', {
-  duration: 2000,
-  origin:'top',
-  distance:'300px'
-});
-sr.reveal('.showcase-right', {
-  duration: 2000,
-  origin:'right',
-  distance:'300px'
-});
-sr.reveal('.showcase-btn', {
-  duration: 2000,
-  delay: 2000,
-  origin:'bottom'
-});
-sr.reveal('#testimonial div', {
-  duration: 2000,
-  origin:'bottom'
-});
+
 sr.reveal('.info-left', {
   duration: 3000,
   origin:'left',
@@ -30,48 +7,47 @@ sr.reveal('.info-left', {
   viewFactor: 0.2
 });
 sr.reveal('.info-left-two', {
-  duration: 4000,
+  duration: 3300,
   origin:'left',
-  distance:'200px',
+  distance:'100px',
 
   viewFactor: 0.2
 });
 sr.reveal('.info-right', {
-  duration: 5000,
+  duration: 3700,
   origin:'left',
-  distance:'300px',
-
+  distance:'100px',
   viewFactor: 0.2
 });
 sr.reveal('.info-right-two', {
-  duration: 6000,
+  duration: 4000,
   origin:'left',
-  distance:'400px',
+  distance:'100px',
   viewFactor: 0.2
 });
 sr.reveal('.image-content.one', {
-  duration: 3000,
+  duration: 1500,
   origin:'left',
-  distance:'250px',
+  distance:'100px',
 });
 sr.reveal('.image-content.two', {
-  duration: 3000,
+  duration: 1500,
   origin:'top',
-  distance:'250px',
+  distance:'200px',
 });
 sr.reveal('.image-content.three', {
-  duration: 3000,
+  duration: 1500,
   origin:'right',
-  distance:'250px',
+  distance:'100px',
 });
 
 sr.reveal('.animate-info-1', {
-  duration: 3000,
+  duration: 2000,
   origin:'left',
-  distance:'250px',
+  distance:'100px',
 });
 sr.reveal('.animate-info-2', {
-  duration: 3000,
+  duration: 2000,
   origin:'right',
-  distance:'250px',
+  distance:'100px',
 });
