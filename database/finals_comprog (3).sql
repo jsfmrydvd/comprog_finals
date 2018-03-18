1d621cb36692da8813dc8c4938c077ad2b98bdad
Josef David <josefmarydavid@gmail.com>
2018-03-18 17:00:11 +0800

    new sql

--- database/finals_comprog (3).sql
+++ database/finals_comprog (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 17, 2018 at 03:56 PM
+-- Generation Time: Mar 18, 2018 at 09:59 AM
 -- Server version: 5.7.21-log
 -- PHP Version: 7.1.8
 
@@ -21,6 +21,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `finals_comprog`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bags`
+--
+
+CREATE TABLE `bags` (
+  `id` int(11) NOT NULL,
+  `product_code` varchar(60) NOT NULL,
+  `product_name` varchar(60) NOT NULL,
+  `product_desc` tinytext NOT NULL,
+  `product_img_name` varchar(60) NOT NULL,
+  `qty` int(5) NOT NULL,
+  `price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bags`
+--
+
+INSERT INTO `bags` (`id`, `product_code`, `product_name`, `product_desc`, `product_img_name`, `qty`, `price`) VALUES
+(23, 'bag-1a', 'Bag-1', 'bag', 'bag-1.jpeg', 18, '5000.00'),
+(24, 'bag-2a', 'Bag-2', 'bag', 'bag-2.jpeg', 11, '200.00'),
+(25, 'bag-3a', 'Bag-3', 'bag', 'bag-3.jpg', 19, '1000.00');
 
 -- --------------------------------------------------------
 
@@ -44,7 +69,7 @@ CREATE TABLE `camera` (
 
 INSERT INTO `camera` (`id`, `product_code`, `product_name`, `product_desc`, `product_img_name`, `qty`, `price`) VALUES
 (19, 'c1', 'cam1', 'camera', 'featured-2.jpeg', 19, '1000.00'),
-(20, 'c2', 'cam2', 'camera', 'camera-2.jpeg', 11, '200.00'),
+(20, 'c2', 'cam2', 'camera', 'camera-2.jpeg', 10, '200.00'),
 (21, 'c3', 'cam3', 'camera', 'camera-1.jpeg', 18, '5000.00'),
 (22, 'c4', 'cam4', 'camera', 'featured-3.jpg', 5, '560.00');
 
@@ -100,39 +125,9 @@ CREATE TABLE `orders` (
 
 INSERT INTO `orders` (`id`, `product_code`, `product_name`, `product_desc`, `price`, `units`, `total`, `date`, `email`) VALUES
 (1, 'w1', 'watch1', 'TEST', 5000, 1, 5000, '2018-03-17 06:02:09', 'root@gmail.com'),
-(2, 'w5', 'watch5', '11', 12321, 1, 12321, '2018-03-17 06:06:04', 'root@gmail.com');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `products`
---
-
-CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
-  `product_code` varchar(60) NOT NULL,
-  `product_name` varchar(60) NOT NULL,
-  `product_desc` tinytext NOT NULL,
-  `product_img_name` varchar(60) NOT NULL,
-  `qty` int(5) NOT NULL,
-  `price` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `products`
---
-
-INSERT INTO `products` (`id`, `product_code`, `product_name`, `product_desc`, `product_img_name`, `qty`, `price`) VALUES
-(1, 'w1', 'watch1', 'TEST', 'watch-1.jpeg', 7, '5000.00'),
-(2, 'w2', 'watch2', 'T', 'watch-2.jpeg', 9, '200.00'),
-(3, 'w3', 'watch3', 'T', 'watch-3.jpeg', 17, '1000.00'),
-(4, 'w4', 'watch4', 'T', 'watch-4.jpeg', 4, '560.00'),
-(5, 'w5', 'watch5', '11', 'watch-5.jpeg', 4, '12321.00'),
-(6, 'w6', 'watch6', '11', 'watch-6.jpeg', 10, '11.00'),
-(7, 'c1', 'cam1', 'TEST', 'featured-2.jpeg', 13, '5000.00'),
-(8, 'c2', 'cam2', 'T', 'camera-2.jpeg', 11, '200.00'),
-(9, 'c3', 'cam3', 'T', 'camera-1.jpeg', 18, '1000.00'),
-(10, 'c4', 'cam4', 'T', 'featured-3.jpg', 5, '560.00');
+(2, 'w5', 'watch5', '11', 12321, 1, 12321, '2018-03-17 06:06:04', 'root@gmail.com'),
+(3, 'w6', 'watch6', '11', 11, 1, 11, '2018-03-18 06:31:56', 'root@gmail.com'),
+(4, 'c2', 'cam2', 'camera', 200, 1, 200, '2018-03-18 06:31:56', 'root@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -206,16 +201,23 @@ CREATE TABLE `watch` (
 --
 
 INSERT INTO `watch` (`id`, `product_code`, `product_name`, `product_desc`, `product_img_name`, `qty`, `price`) VALUES
-(13, 'w1', 'watch1', 'TEST', 'watch-1.jpeg', 18, '5000.00'),
-(14, 'w2', 'watch2', 'T', 'watch-2.jpeg', 11, '200.00'),
-(15, 'w3', 'watch3', 'T', 'watch-3.jpeg', 19, '1000.00'),
-(16, 'w4', 'watch4', 'T', 'watch-4.jpeg', 5, '560.00'),
-(17, 'w5', 'watch5', '11', 'watch-5.jpeg', 4, '12321.00'),
-(18, 'w6', 'watch6', '11', 'watch-6.jpeg', 1, '11.00');
+(13, 'w1', 'watch1', 'watch', 'watch-1.jpeg', 18, '5000.00'),
+(14, 'w2', 'watch2', 'watch', 'watch-2.jpeg', 11, '200.00'),
+(15, 'w3', 'watch3', 'watch', 'watch-3.jpeg', 19, '1000.00'),
+(16, 'w4', 'watch4', 'watch', 'watch-4.jpeg', 5, '560.00'),
+(17, 'w5', 'watch5', 'watch', 'watch-5.jpeg', 4, '1200.00'),
+(18, 'w6', 'watch6', 'watch', 'watch-6.jpeg', 10, '300.00');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `bags`
+--
+ALTER TABLE `bags`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `product_code` (`product_code`);
 
 --
 -- Indexes for table `camera`
@@ -238,13 +240,6 @@ ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `product_code` (`product_code`);
-
---
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -263,6 +258,11 @@ ALTER TABLE `watch`
 --
 
 --
+-- AUTO_INCREMENT for table `bags`
+--
+ALTER TABLE `bags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
 -- AUTO_INCREMENT for table `camera`
 --
 ALTER TABLE `camera`
@@ -276,12 +276,7 @@ ALTER TABLE `clothing`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `users`
 --
